8956d58b54cdaba5f886bb0152a1f00a8e15045c
Christian Benincasa <chrisbenincasa@gmail.com>
2018-05-31 19:04:43 -0400

    Add endpoints for retrieving user lists

--- server/src/api/UsersController.ts
+++ server/src/api/UsersController.ts
@@ -1,26 +1,28 @@
 import * as Router from 'koa-router';
-import { Connection } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 
 import { Controller } from './Controller';
 import * as Entity from '../db/entity'
 
 export class UsersController extends Controller {
     private dbConnection: Connection;
+    private userRepository: Repository<Entity.User>;
 
     constructor(router: Router, connection: Connection) {
         super(router);
         this.dbConnection = connection; // Hang onto a connection to the DB
+        this.userRepository = connection.manager.getRepository(Entity.User);
     }
 
     setupRoutes(): void {
         this.router.get('/users', async (ctx) => {
-            let users = await this.dbConnection.getRepository(Entity.User).find();
+            let users = await this.userRepository.find();
 
             ctx.body = { data: users };
         });
 
         this.router.get('/users/:id', async (ctx) => {
-            let user = await this.dbConnection.getRepository(Entity.User).findOne(ctx.params.id);
+            let user = await this.userRepository.findOne(ctx.params.id);
 
             if (user) {
                 ctx.status = 200;
@@ -30,8 +32,41 @@ export class UsersController extends Controller {
             }
         });
 
-        this.router.put('/users/:id/tracked', async (ctx) => {
-            let user = await this.dbConnection.getRepository(Entity.User).findOne(ctx.params.id);
+        this.router.get('/users/:id/lists', async (ctx) => {
+            let userWithTrackedShows = await this.userRepository.findOne({
+                where: {
+                    id: ctx.params.id
+                },
+                join: {
+                    alias: 'user',
+                    leftJoinAndSelect: {
+                        'movieLists': 'user.movieLists',
+                        'showLists': 'user.showLists'
+                    }
+                }
+            });
+
+            if (userWithTrackedShows) {
+                ctx.status = 200;
+                ctx.body = { data: userWithTrackedShows };
+            } else {
+                ctx.state = 404;
+            }
+        });
+
+        this.router.get('/users/:id/lists/shows/:listId', async (ctx) => {
+            let userWithTrackedShowsQuery = this.userRepository.createQueryBuilder('user').
+                leftJoinAndSelect('user.showLists', 'showLists').
+                where('user.id = :userId', { userId: ctx.params.id }).
+                andWhere('showLists.id = :listId', { listId: ctx.params.listId });
+
+            let userWithTrackedShows = await userWithTrackedShowsQuery.getOne();
+            ctx.status = 200;
+            ctx.body = { data: userWithTrackedShows };
+        });
+
+        this.router.put('/users/:id/tracked/shows', async (ctx) => {
+            let user = await this.userRepository.findOne(ctx.params.id);
 
             if (user) {
                 // await this.dbConnection.getRepository(Entity.Show).insert()
---
+++ server/src/test/user_api.spec.ts
@@ -0,0 +1,75 @@
+import 'mocha';
+
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import { Connection, getConnection, createConnection } from 'typeorm';
+
+import { Show, ShowList, User, MovieList } from '../db/entity';
+import Server from '../Server';
+import { InMemoryDb } from './fixtures/database';
+import * as random from 'random-js';
+
+chai.use(chaiHttp);
+const should = chai.should();
+
+describe('Users API', () => {
+    const r = new random();
+    let server: Server;
+    let connection: Connection;
+    const baseUrl = `http://localhost:3000`
+
+    before(async function () {
+        this.timeout(10000);
+        let db = new InMemoryDb('users_api');
+        server = new Server(3000, db);
+        await server.main();
+        connection = await db.connect();
+    });
+
+    after(function (done) {
+        server.instance.close(() => done());
+    });
+
+    it('should retrieve all lists for a user', async () => {
+        let user = new User('Gordon');
+        let userRet = await connection.getRepository(User).save(user);
+
+        let showList = new ShowList();
+        showList.user = user;
+        let showListRet = await connection.getRepository(ShowList).save(showList);
+
+        let movieList = new MovieList();
+        movieList.user = user;
+        let movieListRet = await connection.getRepository(MovieList).save(movieList);
+
+        let res = await chai.request(baseUrl).get(`/api/v1/users/${userRet.id}/lists`);
+
+        res.type.should.equal('application/json');
+        res.body.should.include.keys('data');
+
+        chai.assert.ownInclude(res.body.data, { id: userRet.id, name: userRet.name });
+        chai.assert.lengthOf(res.body.data.showLists, 1);
+        chai.assert.lengthOf(res.body.data.movieLists, 1);
+    });
+
+    it('should respond with a user\'s specific list', async () => {
+        let user = new User('Gordon');
+        let userRet = await connection.getRepository(User).save(user);
+
+        let show = new Show();
+        show.name = 'Halt and Catch Fire';
+        show.externalId = r.string(12);
+        let showRet = await connection.getRepository(Show).save(show);
+
+        let showList = new ShowList();
+        showList.user = user;
+        showList.shows = [showRet];
+        let showListRet = await connection.getRepository(ShowList).save(showList);
+
+        let res2 = await chai.request(baseUrl).get(`/api/v1/users/${userRet.id}/lists/shows/${showList.id}`)
+
+        res2.type.should.equal('application/json');
+        res2.body.should.include.keys('data');
+        res2.body.data.should.include.keys('name', 'id', 'showLists');
+    });
+});
