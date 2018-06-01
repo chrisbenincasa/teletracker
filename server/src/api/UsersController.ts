66b8ac3c7967e12ed0345bfe04a29d14112bcd7b
Christian Benincasa <chrisbenincasa@gmail.com>
2018-06-01 08:45:25 -0400

    Endpoint for creating lists for a user

--- server/src/api/UsersController.ts
+++ server/src/api/UsersController.ts
@@ -1,8 +1,8 @@
 import * as Router from 'koa-router';
 import { Connection, Repository } from 'typeorm';
 
+import * as Entity from '../db/entity';
 import { Controller } from './Controller';
-import * as Entity from '../db/entity'
 
 export class UsersController extends Controller {
     private dbConnection: Connection;
@@ -15,12 +15,14 @@ export class UsersController extends Controller {
     }
 
     setupRoutes(): void {
+        // Retrieve all users
         this.router.get('/users', async (ctx) => {
             let users = await this.userRepository.find();
 
             ctx.body = { data: users };
         });
 
+        // Retrieve a user
         this.router.get('/users/:id', async (ctx) => {
             let user = await this.userRepository.findOne(ctx.params.id);
 
@@ -32,6 +34,7 @@ export class UsersController extends Controller {
             }
         });
 
+        // Retrieve all lists for a user
         this.router.get('/users/:id/lists', async (ctx) => {
             let userWithTrackedShows = await this.userRepository.findOne({
                 where: {
@@ -54,6 +57,42 @@ export class UsersController extends Controller {
             }
         });
 
+        // Create a list for a user
+        this.router.post('/users/:id/lists', async (ctx) => {
+            let user = await this.userRepository.findOne(ctx.params.id);
+
+            if (!user) {
+                ctx.status = 400;
+                ctx.body = { error: 'Cannot create a list for a user that doesn\'t exist!' };
+            } else if (!ctx.request.body) {
+                ctx.status = 400;
+                ctx.body = { error: 'Invalid request body or request body missing.' };
+            } else {
+                let req = ctx.request.body;
+                if (req.type.toLowerCase() === 'show') {
+                    let listRepo = this.dbConnection.getRepository(Entity.ShowList)
+                    let list = listRepo.create();
+                    list.user = user;
+                    list = await listRepo.save(list);
+
+                    ctx.status = 201;
+                    ctx.body = { data: { id: list.id } };
+                } else if (req.type.toLowerCase() === 'movie') {
+                    let movieRepo = this.dbConnection.getRepository(Entity.MovieList);
+                    let list = movieRepo.create();
+                    list.user = user;
+                    list = await movieRepo.save(list);
+
+                    ctx.status = 201;
+                    ctx.body = { data: { id: list.id } };
+                } else {
+                    ctx.status = 400;
+                    ctx.body = { error: 'Unrecognized list type = ' + req.type };
+                }
+            }
+        });
+
+        // Retrieve a specific show-based list for a user
         this.router.get('/users/:id/lists/shows/:listId', async (ctx) => {
             let userWithTrackedShowsQuery = this.userRepository.createQueryBuilder('user').
                 leftJoinAndSelect('user.showLists', 'showLists').
@@ -63,6 +102,18 @@ export class UsersController extends Controller {
             let userWithTrackedShows = await userWithTrackedShowsQuery.getOne();
             ctx.status = 200;
             ctx.body = { data: userWithTrackedShows };
+        });
+
+        // Retrieve a specific movie-based list for a user
+        this.router.get('/users/:id/lists/movies/:listId', async (ctx) => {
+            let userWithTrackedMoviesQuery = this.userRepository.createQueryBuilder('user').
+                leftJoinAndSelect('user.movieLIsts', 'movieLists').
+                where('user.id = :userId', { userId: ctx.params.id }).
+                andWhere('movieLIsts.id = :listId', { listId: ctx.params.listId });
+
+            let userWithTrackedMovies = await userWithTrackedMoviesQuery.getOne();
+            ctx.status = 200;
+            ctx.body = { data: userWithTrackedMovies };
         });
 
         this.router.put('/users/:id/tracked/shows', async (ctx) => {
