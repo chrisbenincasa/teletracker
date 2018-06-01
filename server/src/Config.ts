962404f6c7ca6c4999683933e1bb2fc5f710f9df
Christian Benincasa <chrisbenincasa@gmail.com>
2018-06-01 12:39:40 -0400

    Add config and logging

---
+++ server/src/Config.ts
@@ -0,0 +1,49 @@
+import { ConnectionOptions } from 'typeorm';
+
+export class ConfigLoader {
+    static load(): Config {
+        const dotenv = require('dotenv');
+        dotenv.config();
+        const config = require('config');
+
+        console.log(config.util.toObject());
+
+        return {
+            server: {
+                port: config.get('server.port')  
+            },
+            logging: {
+                level: (!process.env.NODE_ENV || process.env.NODE_ENV.toLowerCase() !== 'test') ? 'debug' : 'error'
+            },
+            db: {
+                type: config.get('db.type'),
+                host: config.get('db.host'),
+                port: config.get('db.port'),
+                username: config.get('db.username'),
+                password: config.has('db.password') ? config.get('db.password') : null,
+                database: config.get('db.database'),
+                synchronize: config.get('db.synchronize'),
+                logging: config.get('db.logging'),
+                entities: config.get('db.entities'),
+                migrations: config.get('db.migrations'),
+                subscribers: config.get('db.subscribers')
+            }
+        };
+    }
+}
+
+export const GlobalConfig = ConfigLoader.load();
+
+export interface Config {
+    server: ServerConfig;
+    logging: LoggingConfig;
+    db: ConnectionOptions;
+}
+
+export interface LoggingConfig {
+    level: string
+}
+
+export interface ServerConfig {
+    port: number
+}
