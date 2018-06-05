b97f3fe66ba85e1c035ddcfa1117d26faefbfc74
Mark D'Avella <markdavella@gmail.com>
2018-06-05 08:35:05 -0400

    Updating frontend tests

--- frontend/App/Components/FullButton.tsx
+++ frontend/App/Components/FullButton.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { TouchableOpacity, Text } from 'react-native'
 import { FullButtonStyle, FullButtonStyleSheet } from './Styles/FullButtonStyles'
+
 import ExamplesRegistry from '../Services/ExamplesRegistry'
 
 // Note that this file (App/Components/FullButton) needs to be
--- frontend/Tests/Components/FullButton.spec.js
+++ frontend/Tests/Components/FullButton.spec.js
@@ -5,7 +5,8 @@ import { shallow } from 'enzyme'
 import renderer from 'react-test-renderer'
 
 test('FullButton component renders correctly', () => {
-  const tree = renderer.create(<FullButton onPress={() => {}} text='hi' />).toJSON()
+  const tree = renderer.create(<FullButton onPress={() => {}} 
+  text='hi' />).toJSON()
   expect(tree).toMatchSnapshot()
 })
 
@@ -13,9 +14,11 @@ test('onPress', () => {
   let i = 0 // i guess i could have used sinon here too... less is more i guess
   const onPress = () => i++
   const wrapperPress = shallow(<FullButton onPress={onPress} text='hi' />)
-
+  
   expect(wrapperPress.prop('onPress')).toBe(onPress) // uses the right handler
   expect(i).toBe(0)
   wrapperPress.simulate('press')
   expect(i).toBe(1)
 })
+
+
