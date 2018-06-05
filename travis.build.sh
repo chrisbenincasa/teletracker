7ec3aa762382e7fa395239754eccc91fdcd4a5b0
Christian Benincasa <chrisbenincasa@gmail.com>
2018-06-05 09:14:33 -0400

    Get final 2 frontend tests to pass

--- travis.build.sh
+++ travis.build.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-cd $TRAVIS_BUILD_DIR/server
-yarn install -g mocha
+pushd $TRAVIS_BUILD_DIR/server
+yarn global add mocha
 yarn install
 yarn test
+popd
+
+pushd $TRAVIS_BUILD_DIR/frontend
+yarn install
+yarn test
+popd
