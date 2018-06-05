ea9d79e55dae7525730e7c0d088745454ef95833
Christian Benincasa <chrisbenincasa@gmail.com>
2018-06-05 09:16:13 -0400

    No support for pushd on Travis CI wtf

--- travis.build.sh
+++ travis.build.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-pushd $TRAVIS_BUILD_DIR/server
+cd $TRAVIS_BUILD_DIR/server
 yarn global add mocha
 yarn install
 yarn test
-popd
+cd ..
 
-pushd $TRAVIS_BUILD_DIR/frontend
+cd $TRAVIS_BUILD_DIR/frontend
 yarn install
 yarn test
-popd
+cd ..
