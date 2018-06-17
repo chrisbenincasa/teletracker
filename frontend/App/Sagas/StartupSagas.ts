88ceff54c5ba8d44a707111f8f2b0c924b06abfa
Christian Benincasa <chrisbenincasa@gmail.com>
2018-06-17 08:18:34 -0400

    Fix nav issues

--- frontend/App/Sagas/StartupSagas.ts
+++ frontend/App/Sagas/StartupSagas.ts
@@ -1,5 +1,5 @@
 import { Navigation } from 'react-native-navigation';
-import { call, select } from 'redux-saga/effects';
+import { call, select, take } from 'redux-saga/effects';
 import * as NavigationConfig from '../Navigation/NavigationConfig';
 
 export function * startup(): IterableIterator<any> {
@@ -8,6 +8,9 @@ export function * startup(): IterableIterator<any> {
   const isLoggedIn = !!state.user.token;
 
   const view = isLoggedIn ? NavigationConfig.AppStack : NavigationConfig.AuthStack;
+
+  // Waits until Navigation fires "registerAppLaunchedListener"
+  yield take('navigation/registerAppLaunchedListener');
 
   yield call([Navigation, Navigation.setRoot], view);
 }
