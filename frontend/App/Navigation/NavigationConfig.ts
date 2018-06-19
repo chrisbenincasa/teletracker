cd1ca67a68633fd2ff18242d42fa7978f96d3d79
Mark D'Avella <markdavella@gmail.com>
2018-06-19 13:01:54 -0400

    Fixed up bottomTabs

--- frontend/App/Navigation/NavigationConfig.ts
+++ frontend/App/Navigation/NavigationConfig.ts
@@ -1,154 +1,146 @@
 export const AuthStack = {
-  root: {
-    stack: {
-      id: 'Login',
-      children: [{
-        component: {
-          name: 'navigation.main.LoginScreen',
-          options: {
-            topBar: {
-              visible: false
-            }
-          }
+    root: {
+        stack: {
+            id: 'Login',
+            children: [{
+                component: {
+                    name: 'navigation.main.LoginScreen',
+                    options: {
+                        topBar: {
+                            visible: false
+                        }
+                    }
+                }
+            }]
         }
-      }]
     }
-  }
 };
 
-// This is the original ListView
-// This does not display a translucent topBar
-
-// export let ListView = {
-//   component: {
-//       name: 'navigation.main.ListView',
-//       options: {
-//           animated: true,
-//           topBar: {
-//               visible: false
-//           }
-//       }
-//   }
-// }
-
 export let ListView = {
-  bottomTabs: {
-    children: [{
-      stack: {
+    bottomTabs: {
         children: [{
-          component: {
-            name: 'navigation.main.ListView',
-            passProps: {
-              text: 'List View'
-            },
-            options: {
-              bottomTab: {
-                title: 'My List',
-                icon: require('../Images/Icons/list-icon.png'),
-                testID: 'FIRST_TAB_BAR_BUTTON'
-              }
+            stack: {
+                children: [{
+                    component: {
+                        name: 'navigation.main.ListView',
+                            passProps: {
+                                text: 'List View'
+                            },
+                            options: {
+                                bottomTab: {
+                                    title: 'My List',
+                                    icon: require('../Images/Icons/list-icon.png'),
+                                    testID: 'FIRST_TAB_BAR_BUTTON'  
+                                },
+                                topBar: {
+                                    visible: false
+                                }
+                            }
+                        }
+                    }]
+                }
+            }, 
+            {
+            stack: {
+                children: [{
+                    component: {
+                        name: 'navigation.main.SearchScreen',
+                        passProps: {
+                            text: 'Search'
+                        },
+                        options: {
+                            bottomTab: {
+                                title: 'Search',
+                                icon: require('../Images/Icons/search.png'),
+                                testID: 'SECOND_TAB_BAR_BUTTON'
+                            },
+                            topBar: {
+                                visible: false,
+                            }
+                        }
+                    }
+                }]
             }
-          }
-        }]
-      }
-    },
-    {
-      stack: {
-        children: [{
-          component: {
-            name: 'navigation.main.SearchScreen',
-            passProps: {
-              text: 'Search'
-            },
-            options: {
-              bottomTab: {
-                title: 'Search',
-                icon: require('../Images/Icons/search.png'),
-                testID: 'SECOND_TAB_BAR_BUTTON'
-              }
-            }
-          }
-        }]
-      }
-    },
-    {
-      stack: {
-        children: [{
-          component: {
-            name: 'navigation.main.NotificationsScreen',
-            passProps: {
-              text: 'Notifications'
-            },
-            options: {
-              bottomTab: {
-                title: 'Notifications',
-                icon: require('../Images/Icons/notification.png'),
-                testID: 'THIRD_TAB_BAR_BUTTON'
-              }
-            }
-          }
-        }]
-      }
+        },
+        {
+        stack: {
+            children: [{
+                component: {
+                    name: 'navigation.main.NotificationsScreen',
+                    passProps: {
+                        text: 'Notifications'
+                    },
+                    options: {
+                        bottomTab: {
+                            title: 'Notifications',
+                            icon: require('../Images/Icons/notification.png'),
+                            testID: 'THIRD_TAB_BAR_BUTTON'
+                        },
+                        topBar: {
+                            visible: false
+                        }
+                    }
+                }
+            }]
+        }
     }]
   }
 }
 
 export let MenuView = {
-  component: {
-    name: 'navigation.main.MenuScreen',
-    options: {
-      animated: true,
-      topBar: {
-        visible: false
-      }
+    component: {
+        name: 'navigation.main.MenuScreen',
+        options: {
+            animated: true,
+            topBar: {
+                visible: false
+            }
+        }
     }
-  }
 }
 
 export let DetailView = {
-  component: {
-    name: 'navigation.main.ItemDetailScreen',
-    options: {
-      animated: true,
-      topBar: {
-        visible: false
-      },
+    component: {
+        name: 'navigation.main.ItemDetailScreen',
+        options: {
+            animated: true,
+            topBar: {
+                visible: false
+            },
+        }
     }
-  }
 }
 
 export let SearchView = {
-  component: {
-    name: 'navigation.main.SearchScreen',
-    options: {
-      animated: true,
-      topBar: {
-        visible: false
-      },
+    component: {
+        name: 'navigation.main.SearchScreen',
+        options: {
+            animated: true,
+            topBar: {
+                visible: false
+            },
+        }
     }
-  }
 }
 
 export let NotificationsView = {
-  component: {
-    name: 'navigation.main.NotificationsScreen',
-    options: {
-      animated: true,
-      topBar: {
-        visible: false
-      },
+    component: {
+        name: 'navigation.main.NotificationsScreen',
+        options: {
+            animated: true,
+            topBar: {
+                visible: false
+            },
+        }
     }
-  }
 }
 
 // Initial State of the App stack
 export const AppStack = {
-  root: {
-    stack: {
-      id: 'App',
-      children: [
-        ListView
-      ]
+    root: ListView,
+    options: {
+        topBar: {
+            visible: false
+        }
     }
-  }
 };
