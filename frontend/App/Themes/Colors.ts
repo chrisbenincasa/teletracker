6960d9b2902fbf68db9f295ac41151030d3f47c2
Mark D'Avella <markdavella@gmail.com>
2018-06-21 10:59:49 -0400

    Fixed header navigation

--- frontend/App/Themes/Colors.ts
+++ frontend/App/Themes/Colors.ts
@@ -21,7 +21,8 @@ const colors = {
   eggplant: '#251a34',
   border: '#483F53',
   banner: '#5F3E63',
-  text: '#E0D7E5'
+  text: '#E0D7E5',
+  headerBackground: '#496fc2'
 };
 
 export default colors;
