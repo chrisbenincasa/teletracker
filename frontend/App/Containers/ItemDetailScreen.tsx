c4fecb34242cf42c863e9e5c22a603319d04244c
Mark D'Avella <markdavella@gmail.com>
2018-06-25 23:03:55 -0400

    first pass of redesign

--- frontend/App/Containers/ItemDetailScreen.tsx
+++ frontend/App/Containers/ItemDetailScreen.tsx
@@ -1,7 +1,7 @@
 import * as R from 'ramda';
 import React, { Component } from 'react';
-import { Image, KeyboardAvoidingView, Text, View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Image, KeyboardAvoidingView, Text, View, ScrollView } from 'react-native';
+import { Button, Card, Rating, Avatar } from 'react-native-elements';
 import Header from '../Components/Header/Header';
 import { connect } from 'react-redux';
 import { Dispatch } from 'redux';
@@ -12,6 +12,7 @@ import { Thing } from '../Model/external/themoviedb';
 import ListActions from '../Redux/ListRedux';
 import headerStyles from '../Themes/ApplicationStyles';
 import styles from './Styles/ItemDetailScreenStyle';
+import { END } from 'redux-saga';
 
 var MessageBarAlert = require('react-native-message-bar').MessageBar;
 var MessageBarManager = require('react-native-message-bar').MessageBarManager;
@@ -45,12 +46,68 @@ class ItemDetailScreen extends Component<Props> {
 
     getImagePath() {
         let meta = this.props.item.metadata.themoviedb;
+        console.tron.log(this.props.item.metadata.themoviedb);
         if (this.hasTmdbMovie()) {
             return R.view<Props, Movie>(this.tmdbMovieView, this.props).poster_path;
         } else if (this.hasTmdbShow()) {
             return meta.show.poster_path;
         } else if (this.hasTmdbPerson()) {
             return meta.person.profile_path;
+        }
+    }
+
+    getBackdropImagePath() {
+        let meta = this.props.item.metadata.themoviedb;
+        if (this.hasTmdbMovie()) {
+            return R.view<Props, Movie>(this.tmdbMovieView, this.props).backdrop_path;
+        } else if (this.hasTmdbShow()) {
+            return meta.show.backdrop_path; 
+        } else if (this.hasTmdbPerson()) {
+            return meta.person.backdrop_path; //TO DO: Confirm if this exists
+        }
+    }
+
+    getRatingPath() {
+        let meta = this.props.item.metadata.themoviedb;
+        if (this.hasTmdbMovie()) {
+            return R.view<Props, Movie>(this.tmdbMovieView, this.props).vote_average;
+        } else if (this.hasTmdbShow()) {
+            return meta.show.vote_average;
+        } else if (this.hasTmdbPerson()) {
+            return meta.person.vote_average; //TO DO: Confirm if this exists
+        }
+    }
+
+    getVoteCount() {
+        let meta = this.props.item.metadata.themoviedb;
+        if (this.hasTmdbMovie()) {
+            return R.view<Props, Movie>(this.tmdbMovieView, this.props).vote_count;
+        } else if (this.hasTmdbShow()) {
+            return meta.show.vote_count;
+        } else if (this.hasTmdbPerson()) {
+            return meta.person.vote_count; //TO DO: Confirm if this exists
+        }
+    }
+
+    getReleaseYear() {
+        let meta = this.props.item.metadata.themoviedb;
+        if (this.hasTmdbMovie()) {
+            return R.view<Props, Movie>(this.tmdbMovieView, this.props).release_date.substring(0,4);
+        } else if (this.hasTmdbShow()) {
+            return meta.show.release_date.substring(0,4);
+        } else if (this.hasTmdbPerson()) {
+            return meta.person.release_date.substring(0,4); //TO DO: Confirm if this exists
+        }
+    }
+
+    getDescription() {
+        let meta = this.props.item.metadata.themoviedb;
+        if (this.hasTmdbMovie()) {
+            return R.view<Props, Movie>(this.tmdbMovieView, this.props).overview;
+        } else if (this.hasTmdbShow()) {
+            return meta.show.overview;
+        } else if (this.hasTmdbPerson()) {
+            return meta.person.overview; //TO DO: Confirm if this exists
         }
     }
 
@@ -103,12 +160,122 @@ class ItemDetailScreen extends Component<Props> {
                     centerComponent={{title: this.props.item.name,  style: { color: 'white' } }} 
                 />
                 <KeyboardAvoidingView behavior='position'>
-                    <Image source={{ uri: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2' + this.getImagePath()}} style={{width:185,height:278}} />
-                    <Text>{this.props.item.name}</Text>
-                    <Button title='Add' onPress={this.addItem.bind(this)}></Button>
-                </KeyboardAvoidingView>
-                <MessageBarAlert ref="alert" />
+                <View
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                    }}
+                >
+                    <Image source={{ uri: 'https://image.tmdb.org/t/p/w500' + this.getBackdropImagePath()}} style={{
+                        flex: 1, 
+                        width: '100%',
+                        height: '100%',
+                        resizeMode: 'cover'
+                    }} />
+                </View>
+                <View style={{
+                    position: 'relative',
+                    top: 60,
+                    left: 0,
+                    backgroundColor: 'transparent',
+                    flexDirection: 'row'
+                }}>
+                    <Image source={{ uri: 'https://image.tmdb.org/t/p/w92' + this.getImagePath()}} style={{
+                        width: 92,
+                        height: 138, 
+                        marginTop: 20,
+                        marginLeft: 12,
+                        borderWidth: 2,
+                        borderColor: '#fff'
+                    }} />
+                    <View style={{flex: 1, flexDirection: 'column', alignItems: 'flex-start', justifyContent: 'flex-end'}}>
+                    <Text style={{marginTop: 10,marginLeft: 10,fontSize: 20}}>
+                        {this.props.item.name} ({this.getReleaseYear()})
+                    </Text>
+                    <View style={{flexDirection: 'row'}}>
+                        <Rating
+                            type="star"
+                            fractions={1}
+                            startingValue={this.getRatingPath() / 2}
+                            readonly
+                            imageSize={15}
+                            style={{ 
+                                paddingBottom: 15,
+                                marginLeft: 10
+                            }}
+                        />
+                        <Text style={{fontSize: 10,marginLeft: 10,fontStyle: 'italic'}}>
+                            ({this.getVoteCount()})
+                        </Text>
+                    </View>
+                </View>
             </View>
+        </KeyboardAvoidingView>
+        <View style={{marginTop: 60}}>
+            <Card>
+                <Text>{this.getDescription()}</Text>
+            </Card>
+        </View>
+
+        <Button title='Add' onPress={this.addItem.bind(this)}></Button>
+        <Text style={{marginTop: 10, marginLeft: 12, fontSize: 16}}>Starring:</Text>
+        
+        <ScrollView horizontal={true} style={{flexDirection: 'row', marginTop: 5, marginLeft: 12}}>
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+            <Avatar
+                large
+                rounded
+                source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/kfriedson/128.jpg"}}
+                activeOpacity={0.7}
+                containerStyle={{paddingLeft: 5, paddingRight: 5}}
+            />
+        </ScrollView>
+        <MessageBarAlert ref="alert" />
+    </View>
         )
     }
 }
--- frontend/App/Containers/Styles/ItemDetailScreenStyle.ts
+++ frontend/App/Containers/Styles/ItemDetailScreenStyle.ts
@@ -2,5 +2,11 @@ import { StyleSheet } from 'react-native';
 import { ApplicationStyles } from '../../Themes/';
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen
+  ...ApplicationStyles.screen,
+//   CoverImage: {
+//     flex: 1, 
+//     resizeMode: 'cover',
+//     justifyContent: 'center'
+//   },
+//   PosterImage: {}
 });
