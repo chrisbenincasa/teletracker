199b5480778fe32092725d2ab9dc7f5c54f0cbfd
Mark D'Avella <markdavella@gmail.com>
2018-06-26 10:06:48 -0400

    clean up

--- frontend/App/Containers/Styles/ItemDetailScreenStyle.ts
+++ frontend/App/Containers/Styles/ItemDetailScreenStyle.ts
@@ -2,11 +2,64 @@ import { StyleSheet } from 'react-native';
 import { ApplicationStyles } from '../../Themes/';
 
 export default StyleSheet.create({
-  ...ApplicationStyles.screen,
-//   CoverImage: {
-//     flex: 1, 
-//     resizeMode: 'cover',
-//     justifyContent: 'center'
-//   },
-//   PosterImage: {}
+    ...ApplicationStyles.screen,
+    coverContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    },
+    coverImage: {
+        flex: 1, 
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover'
+    },
+    posterImage: {
+        width: 92,
+        height: 138, 
+        marginTop: 20,
+        marginLeft: 12,
+        borderWidth: 2,
+        borderColor: '#fff'
+    },
+    subHeaderContainer: {
+        position: 'relative',
+        top: 60,
+        left: 0,
+        backgroundColor: 'transparent',
+        flexDirection: 'row'
+    },
+    itemDetailsContainer: {
+        flex: 1, 
+        flexDirection: 'column', 
+        alignItems: 'flex-start', 
+        justifyContent: 'flex-end'
+    },
+    ratingsContainer: {
+        flexDirection: 'row'
+    },
+    ratingCount: {
+        fontSize: 10,
+        marginLeft: 10,
+        fontStyle: 'italic'
+    },
+    itemDescriptionContainer: {
+        marginTop: 60
+    },
+    starringHeader: {
+        marginTop: 10,
+        marginLeft: 12,
+        fontSize: 16
+    },
+    avatarContainer: {
+        flexDirection: 'row', 
+        marginTop: 5, 
+        marginLeft: 12
+    },
+    avatar: {
+        paddingLeft: 5,
+        paddingRight: 5
+    }
 });
