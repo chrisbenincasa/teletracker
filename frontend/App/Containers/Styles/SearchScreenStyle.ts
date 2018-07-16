cfe03205bc5707d117ca44c6c881bb96b5f56c6a
Mark D'Avella <markdavella@gmail.com>
2018-07-16 16:20:11 -0400

    lil clean up

--- frontend/App/Containers/Styles/SearchScreenStyle.ts
+++ frontend/App/Containers/Styles/SearchScreenStyle.ts
@@ -11,15 +11,6 @@ export default StyleSheet.create({
         opacity: .60,
         backgroundColor: '#000'
     },
-    posterContainer: {
-        flex: 1,
-        // width: 110,
-        // height: 153,
-        width: 175,
-        height: 243,
-        backgroundColor: '#C9C9C9',
-        alignContent: 'center'
-    },
     defaultScreen: {
         flex: 1,
         flexDirection: 'column',
