90347447cd3068fc531b3fff8898a719f3045b44
Christian Benincasa <chrisbenincasa@gmail.com>
2018-07-19 10:55:59 -0400

    Fix deploy script (maybe) [skip ci]

--- docker_push.sh
+++ docker_push.sh
@@ -7,7 +7,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 cd scala-server
 
 TAG="${TRAVIS_TAG:-SNAPSHOT}"
-VERSION=`sbt -Drevision=$TAG --error 'set showSuccess := false' "showVersion"`
+VERSION=$(sbt -Drevision=$TAG --error 'set showSuccess := false' "showVersion")
 
 echo "Pushing version v${VERSION}"
 
@@ -17,8 +17,5 @@ if [[ -z "$SKIP_DOCKER_BUILD" ]]; then
     sbt ++$SCALA_V -Drevision=${TAG} docker
 fi
 
-if [[ "$TAG" = "SNAPSHOT" ]]; then
-    docker push chrisbenincasa/teletracker:latest
-else 
-    docker push chrisbenincasa/teletracker:v${VERSION}
-fi
+docker push chrisbenincasa/teletracker:latest
+docker push chrisbenincasa/teletracker:v${VERSION}
