8881b784ffaa493551c60a3993e7e72dc910d803
Christian Benincasa <chrisbenincasa@gmail.com>
2018-07-19 08:16:18 -0400

    Attempt to build when tagged

--- docker_push.sh
+++ docker_push.sh
@@ -4,5 +4,12 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 cd scala-server
-sbt ++$TRAVIS_SCALA_VERSION docker
-docker push chrisbenincasa/teletracker:latest
+TAG="${TRAVIS_TAG:-SNAPSHOT}"
+VERSION=`sbt -Drevision=$TAG --error 'set showSuccess := false' "showVersion"`
+sbt ++$TRAVIS_SCALA_VERSION -Drevision=${TAG} docker
+if [ "$TAG" = "SNAPSHOT"] 
+then
+    docker push chrisbenincasa/teletracker:latest
+else
+    docker push chrisbenincasa/teletracker:${TAG}
+fi
