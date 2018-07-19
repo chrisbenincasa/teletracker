6004e2144767e6274c55b2ab154f6e260246825f
Christian Benincasa <chrisbenincasa@gmail.com>
2018-07-18 20:55:26 -0400

    Test deploy stage

--- docker_push.sh
+++ docker_push.sh
@@ -4,5 +4,5 @@ set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 cd scala-server
-travis_retry sbt ++$TRAVIS_SCALA_VERSION docker
+sbt ++$TRAVIS_SCALA_VERSION docker
 docker push chrisbenincasa/teletracker:latest
