74715b924e21ce7465e396ddd2f2e412968f0265
Christian Benincasa <chrisbenincasa@gmail.com>
2018-07-21 14:17:59 -0400

    Fix events spec

--- scala-server/src/test/scala/com/chrisbenincasa/services/teletracker/testing/EventsSpec.scala
+++ scala-server/src/test/scala/com/chrisbenincasa/services/teletracker/testing/EventsSpec.scala
@@ -1,7 +1,7 @@
 package com.chrisbenincasa.services.teletracker.testing
 
 import com.chrisbenincasa.services.teletracker.auth.jwt.JwtVendor
-import com.chrisbenincasa.services.teletracker.controllers.AddUserEventRequest
+import com.chrisbenincasa.services.teletracker.controllers.EventCreate
 import com.chrisbenincasa.services.teletracker.db.UsersDbAccess
 import com.chrisbenincasa.services.teletracker.db.model.Event
 import com.chrisbenincasa.services.teletracker.model.DataResponse
@@ -9,7 +9,6 @@ import com.chrisbenincasa.services.teletracker.testing.framework.BaseSpecWithSer
 import com.chrisbenincasa.services.teletracker.util.json.circe._
 import io.circe.parser._
 import io.circe.generic.auto._
-import java.sql.Timestamp
 
 class EventsSpec extends BaseSpecWithServer {
   "Events" should "should create and read events for a user" in {
@@ -19,7 +18,7 @@ class EventsSpec extends BaseSpecWithServer {
 
     server.httpPost(
       "/api/v1/users/self/events",
-      serializer.writeValueAsString(AddUserEventRequest("self", Event(None, "Watched", "show", "123", None, userId, new Timestamp(System.currentTimeMillis())), null)),
+      serializer.writeValueAsString(EventCreate("Watched", "show", "123", None, System.currentTimeMillis())),
       headers = Map("Authorization" -> s"Bearer $jwt")
     )
 
