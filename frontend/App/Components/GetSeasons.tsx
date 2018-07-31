597346598399e0da9f94796c15a632626a4557c1
Mark D'Avella <markdavella@gmail.com>
2018-07-31 16:25:05 -0400

    more paper work

--- frontend/App/Components/GetSeasons.tsx
+++ frontend/App/Components/GetSeasons.tsx
@@ -1,7 +1,8 @@
 
 import React, { Component } from 'react';
-import { Text, View, ScrollView } from 'react-native';
-import { Avatar, Divider } from 'react-native-elements';
+import { Text, View } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import { ListAccordion, ListItem, Divider } from 'react-native-paper';
 import { Thing } from '../Model/external/themoviedb';
 import getMetadata from './Helpers/getMetadata';
 import { parseInitials } from './Helpers/textHelper';
@@ -23,33 +24,46 @@ export default class GetSeasons extends Component {
                 <View style={styles.seasonsContainer}>
                     <Divider style={styles.divider} />
                     <Text style={styles.seasonsHeader}>Season Guide:</Text>
-                    <ScrollView
-                        horizontal={true}
-                        showsHorizontalScrollIndicator={false}
+                    <View
                         style={styles.avatarContainer}>
                         {
                             getMetadata.getSeasons(this.props.item) ? getMetadata.getSeasons(this.props.item).map((i) => (
-                                <View key={i.id}>
-                                    <Avatar
-                                        key={i.id}
-                                        large
-                                        rounded
-                                        source={
-                                            i.poster_path
-                                                ? {
-                                                    uri: "https://image.tmdb.org/t/p/w92" + i.poster_path
-                                                }
-                                                : null}
-                                        activeOpacity={0.7}
-                                        title={i.poster_path ? null : parseInitials(i.name)}
-                                        titleStyle={parseInitials(i.name).length > 2 ? { fontSize: 26 } : null}
-                                    />
-                                    <Text style={styles.seasonsName}>{i.name}</Text>
-                                </View>
+                                
+                                <ListAccordion
+                                    title="Accordion"
+                                    icon={ 
+                                        i.poster_path ? { 
+                                            uri: 'https://image.tmdb.org/t/p/w92' + i.poster_path 
+                                        } : 'broken-image' 
+                                    }
+                                    key={i.id}
+                                >
+                                    <ListItem title="Episode 1" />
+                                    <ListItem title="Episode 2" />
+                                </ListAccordion>
+                                
+                                // <View key={i.id}>
+                                //     <Avatar
+                                //         key={i.id}
+                                //         large
+                                //         rounded
+                                //         // {{ uri: 'https://avatars0.githubusercontent.com/u/17571969?v=3&s=400' }}
+                                //         source={
+                                //             i.poster_path
+                                //                 ? {
+                                //                     uri: "https://image.tmdb.org/t/p/w92" + i.poster_path
+                                //                 }
+                                //                 : null}
+                                //         activeOpacity={0.7}
+                                //         title={i.poster_path ? null : parseInitials(i.name)}
+                                //         titleStyle={parseInitials(i.name).length > 2 ? { fontSize: 26 } : null}
+                                //     />
+                                //     <Text style={styles.seasonsName}>{i.name}</Text>
+                                // </View>
                             ))
                                 : null
                         }
-                    </ScrollView>
+                    </View>
                 </View>
             : null
         )
