333f9b5fe199900c89154a9bfbb11c590a70e081
Christian Benincasa <chrisbenincasa@gmail.com>
2019-04-15 12:13:18 -0600

    Hookup initial auth dispatch thing

--- web/src/containers/app/index.tsx
+++ web/src/containers/app/index.tsx
@@ -1,20 +1,57 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import Home from '../home'
 import About from '../about'
+import { connect } from 'react-redux'
+import { Dispatch, bindActionCreators } from 'redux';
+import { checkAuth } from '../../modules/counter';
 
-const App = () => (
-  <div>
-    <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
-    </header>
+// const App = () => (
+  
+// )
 
-    <main>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about-us" component={About} />
-    </main>
-  </div>
-)
+interface OwnProps {
+  
+}
 
-export default App
+interface DispatchProps {
+  checkAuth: () => void
+}
+
+type Props = DispatchProps & OwnProps
+
+class App extends Component<Props> {
+  componentDidMount() {
+    this.props.checkAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/about-us">About</Link>
+        </header>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about-us" component={About} />
+        </main>
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps: (dispatch: Dispatch) => DispatchProps = (dispatch) => {
+  return bindActionCreators(
+    {
+      checkAuth
+    },
+    dispatch
+  )
+}
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
