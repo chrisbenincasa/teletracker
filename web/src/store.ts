08382edcce2b66ae7fc40cf3ece02bf6e3d35b5a
Christian Benincasa <chrisbenincasa@gmail.com>
2019-04-15 11:12:42 -0600

    Hook up redux-persist

--- web/src/store.ts
+++ web/src/store.ts
@@ -1,5 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
+import { persistStore, persistReducer } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import thunk from 'redux-thunk'
 import createHistory from 'history/createBrowserHistory'
 import rootReducer from './reducers/'
@@ -9,6 +11,11 @@ export const history = createHistory()
 const initialState = {}
 const enhancers: any[] = []
 const middleware = [thunk, routerMiddleware(history)]
+
+const persistConfig = {
+  key: "root",
+  storage
+};
 
 if (process.env.NODE_ENV === 'development') {
   const devToolsExtension = (window as any).__REDUX_DEVTOOLS_EXTENSION__
@@ -23,8 +30,12 @@ const composedEnhancers = compose(
   ...enhancers
 )
 
-export default createStore(
-  connectRouter(history)(rootReducer),
-  initialState,
-  composedEnhancers
-)
+const reducerWithHistory = connectRouter(history)(rootReducer);
+
+const persistedReducer = persistReducer(persistConfig, reducerWithHistory);
+
+export default () => {
+  let store = createStore(persistedReducer, initialState, composedEnhancers);
+  let persistor = persistStore(store);
+  return { store, persistor };
+}
