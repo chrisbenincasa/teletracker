68042362fc911d4b19bc168f8ebc6ff2f462a8f7
Christian Benincasa <chrisbenincasa@gmail.com>
2019-04-16 10:03:38 -0600

    Refactor based on feedback

--- web/src/reducers/index.ts
+++ web/src/reducers/index.ts
@@ -1,12 +1,11 @@
 import { combineReducers } from 'redux';
-import counter from './counter';
 import auth, { State as AuthState } from './auth';
 
+// A type that represents the entire app state
 export interface AppState {
   auth: AuthState;
 }
 
 export default combineReducers({
-  counter,
   auth,
 });
