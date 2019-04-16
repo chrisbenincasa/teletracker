43bb69c8c3521e4fa7649a5c6e4bd56608bb3803
Mark D'Avella <markdavella@gmail.com>
2019-04-15 21:20:19 -0400

    structure formatting and stuff

---
+++ web/src/reducers/counter.ts
@@ -0,0 +1,40 @@
+import * as constants from '../constants';
+
+const initialState = {
+  count: 0,
+  isIncrementing: false,
+  isDecrementing: false,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case constants.INCREMENT_REQUESTED:
+      return {
+        ...state,
+        isIncrementing: true,
+      };
+
+    case constants.INCREMENT:
+      return {
+        ...state,
+        count: state.count + 1,
+        isIncrementing: !state.isIncrementing,
+      };
+
+    case constants.DECREMENT_REQUESTED:
+      return {
+        ...state,
+        isDecrementing: true,
+      };
+
+    case constants.DECREMENT:
+      return {
+        ...state,
+        count: state.count - 1,
+        isDecrementing: !state.isDecrementing,
+      };
+
+    default:
+      return state;
+  }
+};
