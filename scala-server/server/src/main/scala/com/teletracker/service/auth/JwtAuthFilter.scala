13ac87c92463628fe9379d8392b6c2ee6b6a52b9
Christian Benincasa <chrisbenincasa@gmail.com>
2019-05-31 20:56:39 -0600

    Fix logout when token is expired

--- scala-server/server/src/main/scala/com/teletracker/service/auth/JwtAuthFilter.scala
+++ scala-server/server/src/main/scala/com/teletracker/service/auth/JwtAuthFilter.scala
@@ -6,42 +6,74 @@ import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.util.logging.Logger
 import com.twitter.util.{Future, Promise}
-import io.jsonwebtoken.{Jwts, SignatureException}
+import io.jsonwebtoken.{Claims, Jws, Jwts, SignatureException}
 import javax.inject.Inject
-import org.joda.time.DateTime
 import java.time.OffsetDateTime
 import scala.concurrent.{ExecutionContext, Future => SFuture}
 import scala.util.{Failure, Success, Try}
 
-class JwtAuthFilter @Inject()(
-  config: TeletrackerConfig,
-  usersDbAccess: UsersDbAccess
-)(implicit executionContext: ExecutionContext)
-    extends SimpleFilter[Request, Response] {
-  private val logger = Logger(getClass)
+object JwtAuthFilter {
   private val AuthHeaderRegex = """(\S+)\s+(\S+)""".r
 
-  override def apply(
-    request: Request,
-    service: Service[Request, Response]
-  ): Future[Response] = {
-    val token = request.params
+  def extractAuthHeaderValue(
+    header: String,
+    scheme: String
+  ): Option[String] = {
+    header match {
+      case AuthHeaderRegex(s, v) if s.equalsIgnoreCase(scheme) => Some(v)
+      case _                                                   => None
+    }
+  }
+}
+
+class JwtAuthExtractor @Inject()(config: TeletrackerConfig) {
+  import com.teletracker.service.auth.JwtAuthFilter.extractAuthHeaderValue
+
+  def extractToken(request: Request): Option[String] = {
+    request.params
       .get("token")
       .orElse(
         request.headerMap
           .get("Authorization")
           .flatMap(extractAuthHeaderValue(_, "bearer"))
       )
+  }
 
-    token match {
+  def parseToken(token: String): Try[Jws[Claims]] = {
+    Try {
+      Jwts
+        .parser()
+        .setSigningKey(config.auth.jwt.secret.getBytes())
+        .parseClaimsJws(token)
+    }
+  }
+
+  def extractAndParse(request: Request): Try[Jws[Claims]] = {
+    extractToken(request) match {
+      case Some(token) => parseToken(token)
+      case None        => Failure(TokenNotFoundException)
+    }
+  }
+}
+
+object TokenNotFoundException extends Exception("No token found in request")
+
+class JwtAuthFilter @Inject()(
+  config: TeletrackerConfig,
+  usersDbAccess: UsersDbAccess,
+  extractor: JwtAuthExtractor
+)(implicit executionContext: ExecutionContext)
+    extends SimpleFilter[Request, Response] {
+  private val logger = Logger(getClass)
+
+  override def apply(
+    request: Request,
+    service: Service[Request, Response]
+  ): Future[Response] = {
+    extractor.extractToken(request) match {
       case None => Future.value(Response(Status.Unauthorized))
       case Some(t) =>
-        Try {
-          Jwts
-            .parser()
-            .setSigningKey(config.auth.jwt.secret.getBytes())
-            .parseClaimsJws(t)
-        } match {
+        extractor.parseToken(t) match {
           case Success(parsed) =>
             val respPromise = Promise[Response]()
             val foundUserFut = usersDbAccess.getToken(t).flatMap {
@@ -77,16 +109,6 @@ class JwtAuthFilter @Inject()(
             logger.error(e.getMessage, e)
             Future.value(Response(Status.InternalServerError))
         }
-    }
-  }
-
-  private def extractAuthHeaderValue(
-    header: String,
-    scheme: String
-  ): Option[String] = {
-    header match {
-      case AuthHeaderRegex(s, v) if s.equalsIgnoreCase(scheme) => Some(v)
-      case _                                                   => None
     }
   }
 }
--- scala-server/server/src/main/scala/com/teletracker/service/controllers/AuthController.scala
+++ scala-server/server/src/main/scala/com/teletracker/service/controllers/AuthController.scala
@@ -2,19 +2,29 @@ package com.teletracker.service.controllers
 
 import com.teletracker.service.auth.RequestContext._
 import com.teletracker.service.auth.jwt.JwtVendor
-import com.teletracker.service.auth.{JwtAuthFilter, PasswordAuthFilter}
+import com.teletracker.service.auth.{
+  JwtAuthExtractor,
+  JwtAuthFilter,
+  PasswordAuthFilter,
+  TokenNotFoundException
+}
 import com.teletracker.service.config.TeletrackerConfig
 import com.teletracker.service.db.UsersDbAccess
 import com.teletracker.service.model.DataResponse
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
+import com.twitter.finatra.http.response.ResponseBuilder
+import io.jsonwebtoken.ExpiredJwtException
 import javax.inject.Inject
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success}
 
 class AuthController @Inject()(
   config: TeletrackerConfig,
   usersDbAccess: UsersDbAccess,
-  jwtVendor: JwtVendor
+  jwtVendor: JwtVendor,
+  jwtAuthExtractor: JwtAuthExtractor
 )(implicit executionContext: ExecutionContext)
     extends Controller {
 
@@ -39,13 +49,39 @@ class AuthController @Inject()(
     }
 
     // Log current user out
-    filter[JwtAuthFilter].post("/logout") { req: Request =>
-      // Revoke token?
-      usersDbAccess
-        .revokeToken(req.authContext.user.id, req.authToken.token)
-        .map(_ => {
-          response.ok
-        })
+    post("/logout") { request: Request =>
+      def revoke(
+        userId: Int,
+        token: String
+      ): Future[ResponseBuilder#EnrichedResponse] = {
+        usersDbAccess
+          .revokeToken(userId, token)
+          .map(_ => {
+            response.ok
+          })
+      }
+
+      jwtAuthExtractor.extractToken(request) match {
+        case None => Future.successful(response.badRequest)
+        case Some(token) =>
+          jwtAuthExtractor.parseToken(token) match {
+            case Success(value) =>
+              usersDbAccess.findByEmail(value.getBody.getSubject).flatMap {
+                case None       => Future.successful(response.badRequest)
+                case Some(user) => revoke(user.id.get, token)
+              }
+
+            case Failure(ex: ExpiredJwtException) =>
+              usersDbAccess.findByEmail(ex.getClaims.getSubject).flatMap {
+                case None       => Future.successful(response.badRequest)
+                case Some(user) => revoke(user.id.get, token)
+              }
+
+            case Failure(NonFatal(e)) =>
+              Future.successful(response.internalServerError)
+            case Failure(ex) => throw ex
+          }
+      }
     }
   }
 }
