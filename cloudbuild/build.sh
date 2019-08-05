a2f80aae831f529d8435d4279aa907dd9fafba9f
Christian Benincasa <chrisbenincasa@gmail.com>
2019-08-05 08:23:00 -0600

    Updates to build

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+cp ../scala-server/build.sbt build.sbt
+rsync -av --progress ../scala-server/project . --exclude=target/ --exclude=.bloop
+
+gcloud builds submit . --config=sbt-cloudbuild.yaml
+
+rm build.sbt
+rm -rf project/
