27faa8dead6241cbdf91c64832e20b4f54ed3213
Christian Benincasa <chrisbenincasa@gmail.com>
2019-08-05 23:18:20 -0600

    TF v1

---
+++ terraform/qa/cloudbuild.tf
@@ -0,0 +1,10 @@
+resource "google_cloudbuild_trigger" "push_to_master_trigger" {
+  description = "Push to master"
+
+  trigger_template {
+    branch_name = "master"
+    repo_name   = "github_chrisbenincasa_teletracker"
+  }
+
+  filename = "cloudbuild.yaml"
+}
