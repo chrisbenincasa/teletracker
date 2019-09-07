4b367c348029c05ee81506f9c728589d49bef08b
Christian Benincasa <chrisbenincasa@gmail.com>
2019-09-07 09:22:21 -0600

    Source cast entirely from DB

--- web/src/components/Cast.tsx
+++ web/src/components/Cast.tsx
@@ -9,12 +9,8 @@ import {
 } from '@material-ui/core';
 import { CastMember } from '../types';
 import React, { Component } from 'react';
-import { getMetadataPath } from '../utils/metadata-access';
 import { parseInitials } from '../utils/textHelper';
 import RouterLink from './RouterLink';
-import * as R from 'ramda';
-import _ from 'lodash';
-import { Person } from 'themoviedb-client-typed';
 import Thing from '../types/Thing';
 
 const styles = (theme: Theme) =>
@@ -64,52 +60,31 @@ class Cast extends Component<Props, {}> {
     super(props);
   }
 
-  renderAvatar(person: Person, memberById: { [id: string]: CastMember }) {
+  renderAvatar(person: CastMember) {
     let { classes } = this.props;
 
-    let avatar = (
-      <Avatar
-        alt={person.name}
-        src={
-          person.profile_path
-            ? `https://image.tmdb.org/t/p/w185/${person.profile_path}`
-            : ''
-        }
-        className={classes.avatar}
-      >
-        {person.profile_path ? null : parseInitials(person.name!, 'name')}
-      </Avatar>
-    );
-
-    let dbPerson = memberById[person.id.toString()];
-
-    if (dbPerson) {
-      return (
-        <RouterLink
-          to={'/person/' + dbPerson.slug}
-          className={classes.avatarLink}
+    return (
+      <RouterLink to={'/person/' + person.slug} className={classes.avatarLink}>
+        <Avatar
+          alt={person.name}
+          src={
+            person.profilePath
+              ? `https://image.tmdb.org/t/p/w185/${person.profilePath}`
+              : ''
+          }
+          className={classes.avatar}
         >
-          {avatar}
-        </RouterLink>
-      );
-    } else {
-      return avatar;
-    }
+          {person.profilePath ? null : parseInitials(person.name!, 'name')}
+        </Avatar>
+      </RouterLink>
+    );
   }
 
   render() {
     const { classes, itemDetail } = this.props;
-    const credits = Object(getMetadataPath(itemDetail, 'credits'));
+    const credits = itemDetail.cast;
 
-    let castByTmdbId = R.mapObjIndexed(
-      (c: CastMember[]) => R.head(c)!,
-      R.groupBy(
-        (c: CastMember) => c.tmdbId!,
-        R.filter(c => _.negate(_.isUndefined)(c.tmdbId), itemDetail.cast || []),
-      ),
-    );
-
-    return credits && credits.cast && credits.cast.length > 0 ? (
+    return credits && credits.length > 0 ? (
       <React.Fragment>
         <div className={classes.castContainer}>
           <Typography color="inherit" variant="h5">
@@ -117,9 +92,9 @@ class Cast extends Component<Props, {}> {
           </Typography>
 
           <Grid container className={classes.grid}>
-            {credits.cast.map(person => (
+            {credits.map(person => (
               <div className={classes.personContainer} key={person.id}>
-                {this.renderAvatar(person, castByTmdbId)}
+                {this.renderAvatar(person)}
                 <Typography
                   variant="subtitle1"
                   color="inherit"
@@ -134,7 +109,7 @@ class Cast extends Component<Props, {}> {
                   className={classes.characterName}
                   align="center"
                 >
-                  {person.character}
+                  {person.characterName}
                 </Typography>
               </div>
             ))}
