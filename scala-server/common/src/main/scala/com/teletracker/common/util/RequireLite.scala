e0f151c21464e622a4a52ebd39c13421f7b62994
Christian Benincasa <chrisbenincasa@gmail.com>
2019-09-14 14:59:03 -0600

    More resilient tasks

--- scala-server/common/src/main/scala/com/teletracker/common/util/RequireLite.scala
+++ scala-server/common/src/main/scala/com/teletracker/common/util/RequireLite.scala
@@ -1,3 +1,15 @@
 package com.teletracker.common.util
 
-object RequireLite {}
+import scala.util.control.NoStackTrace
+
+object RequireLite {
+  @inline def requireLite(
+    requirement: Boolean,
+    message: => Any
+  ): Unit = {
+    if (!requirement) {
+      throw new IllegalArgumentException(s"requirement failed: $message")
+      with NoStackTrace
+    }
+  }
+}
