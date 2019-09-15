f85e2d0e9e281337a4c9a4a03e03b552c14d5017
Christian Benincasa <chrisbenincasa@gmail.com>
2019-09-15 12:16:27 -0600

    Auto deploy attempt 1

--- scala-server/server/src/main/scala/com/teletracker/service/auth/AdminFilter.scala
+++ scala-server/server/src/main/scala/com/teletracker/service/auth/AdminFilter.scala
@@ -5,6 +5,7 @@ import com.twitter.finagle.{Service, SimpleFilter}
 import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.Future
 import javax.inject.Inject
+import java.util.Base64
 import scala.concurrent.ExecutionContext
 
 class AdminFilter @Inject()(
@@ -16,18 +17,27 @@ class AdminFilter @Inject()(
     request: Request,
     service: Service[Request, Response]
   ): Future[Response] = {
-    request.headerMap.get("Authorization") match {
-      case Some(header) =>
-        JwtAuthFilter.extractAuthHeaderValue(header, "bearer") match {
-          case Some(token)
-              if teletrackerConfig.auth.admin.adminKeys.contains(token) =>
-            service(request)
+    getParamOrHeader(request) match {
+      case Some(token)
+          if teletrackerConfig.auth.admin.adminKeys.contains(token) =>
+        service(request)
 
-          case _ =>
-            Future.value(Response(Status.Unauthorized))
-        }
-
-      case None => Future.value(Response(Status.Unauthorized))
+      case _ =>
+        Future.value(Response(Status.Unauthorized))
     }
+  }
+
+  private def getParamOrHeader(request: Request): Option[String] = {
+    Option(request.getParam("admin_key"))
+      .map(base64Encoded => {
+        new String(Base64.getDecoder.decode(base64Encoded))
+      })
+      .orElse {
+        request.headerMap
+          .get("Authorization")
+          .flatMap(header => {
+            JwtAuthFilter.extractAuthHeaderValue(header, "bearer")
+          })
+      }
   }
 }
