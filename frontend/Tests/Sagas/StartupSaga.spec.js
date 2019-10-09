6b04228cb1d586c11d3b9f97f9ffcc66a697b2d4
Christian Benincasa <chrisbenincasa@gmail.com>
2019-10-09 09:16:56 -0600

    Remove old mobile code

--- frontend/Tests/Sagas/StartupSaga.spec.js
+++
@@ -1,9 +0,0 @@
-const stepper = (fn) => (mock) => fn.next(mock).value
-
-test('watches for the right action', () => {
-  // How the hell?
-  // const step = stepper(startup())
-  // UserActions.userRequest('1')
-  // expect(step()).toEqual(select(selectAvatar))
-  // expect(step()).toEqual(put(GithubActions.userRequest('GantMan')))
-})
