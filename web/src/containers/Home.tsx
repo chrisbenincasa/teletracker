9436049ebc5ba387d4859442fe130eb4d3c1e41d
Mark D'Avella <markdavella@gmail.com>
2019-10-29 12:59:36 -0400

    Search section on homepage

--- web/src/containers/Home.tsx
+++ web/src/containers/Home.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   createStyles,
+  Divider,
   Grid,
   Theme,
   Typography,
@@ -21,6 +22,8 @@ import { AppState } from '../reducers';
 import { layoutStyles } from '../styles';
 import { Item } from '../types/v2/Item';
 import Odometer from 'react-odometerjs';
+import Typist from 'react-typist';
+import 'react-typist/dist/Typist.css';
 import 'odometer/themes/odometer-theme-default.css';
 import AuthDialog from '../components/Auth/AuthDialog';
 import { retrievePopular } from '../actions/popular';
@@ -49,6 +52,18 @@ const styles = (theme: Theme) =>
         margin: 5,
       },
     },
+    searchContainer: {
+      display: 'flex',
+      flexGrow: 1,
+      flexDirection: 'row',
+      margin: 20,
+      alignItems: 'center',
+      justifyContent: 'center',
+      [theme.breakpoints.down('sm')]: {
+        flexDirection: 'column-reverse',
+        margin: 5,
+      },
+    },
     ctaContainer: {
       display: 'flex',
       flexDirection: 'column',
@@ -58,6 +73,17 @@ const styles = (theme: Theme) =>
         alignItems: 'center',
         textAlign: 'center',
       },
+    },
+    searchCtaContainer: {
+      display: 'flex',
+      flexDirection: 'column',
+      margin: 100,
+      [theme.breakpoints.down('sm')]: {
+        margin: 0,
+        alignItems: 'center',
+        textAlign: 'center',
+      },
+      alignItems: 'center',
     },
     gridContainer: {
       width: '20%',
@@ -95,6 +121,10 @@ const styles = (theme: Theme) =>
       maxWidth: 200,
       whiteSpace: 'nowrap',
     },
+    wrapper: {
+      display: 'flex',
+      flexDirection: 'column',
+    },
   });
 
 interface OwnProps extends WithStyles<typeof styles> {}
@@ -130,6 +160,8 @@ interface State {
   numberMovies: number;
   authModalOpen: boolean;
   authModalScreen?: 'login' | 'signup';
+  activeSearchText: string[];
+  unusedSearchText: string[];
 }
 
 class Home extends Component<Props, State> {
@@ -140,6 +172,8 @@ class Home extends Component<Props, State> {
       numberMovies: 0,
       authModalOpen: false,
       authModalScreen: 'login',
+      activeSearchText: ["90's movies starring Keanu Reeves"],
+      unusedSearchText: [],
     };
   }
 
@@ -184,75 +218,187 @@ class Home extends Component<Props, State> {
     }
   };
 
-  render() {
+  renderTotalMoviesSection = () => {
     const { classes, popular, userSelf, thingsBySlug } = this.props;
+
+    return (
+      <div className={classes.container}>
+        <div className={classes.gridContainer}>
+          <Grid container spacing={1}>
+            {popular &&
+              popular.map(result => {
+                let thing = thingsBySlug[result];
+                return (
+                  <ItemCard
+                    key={result}
+                    userSelf={userSelf}
+                    item={thing}
+                    gridProps={{ xs: 6, sm: 6, md: 6, lg: 6 }}
+                    hoverAddToList={false}
+                    hoverDelete={false}
+                    hoverWatch={false}
+                  />
+                );
+              })}
+          </Grid>
+        </div>
+        <div className={classes.ctaContainer}>
+          <div className={classes.movieCountContainer}>
+            <Typography
+              color="secondary"
+              variant="h2"
+              className={classes.movieCount}
+            >
+              <Odometer
+                value={this.state.numberMovies}
+                format="(,ddd)"
+                theme="default"
+                duration={5000}
+                animation="count"
+              />
+            </Typography>
+            <Typography variant="h2"> movies &amp; counting.</Typography>
+          </div>
+          <Typography variant="h4" color="textSecondary">
+            Discover what you're not watching.
+          </Typography>
+          <div className={classes.buttonContainer}>
+            <Button
+              size="small"
+              variant="outlined"
+              aria-label="Signup"
+              onClick={() => this.toggleAuthModal('signup')}
+              color="secondary"
+              className={classes.signupButton}
+            >
+              <PersonAdd className={classes.buttonIcon} />
+              Signup
+            </Button>
+            <Button
+              size="small"
+              variant="outlined"
+              aria-label="Login"
+              onClick={() => this.toggleAuthModal('login')}
+              className={classes.loginButton}
+            >
+              <ExitToApp className={classes.buttonIcon} />
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  getSearchText = (): void => {
+    const { unusedSearchText } = this.state;
+    const randIndex: number = Number(
+      Math.floor(Math.random() * unusedSearchText.length),
+    );
+    const updatedUnused: string[] = unusedSearchText
+      .slice(0, randIndex)
+      .concat(unusedSearchText.slice(randIndex + 1, unusedSearchText.length));
+    const chosenText: string[] = [unusedSearchText[randIndex]];
+
+    this.setState({
+      activeSearchText: chosenText,
+      unusedSearchText: updatedUnused,
+    });
+  };
+
+  resetSearchText = (): void => {
+    const { unusedSearchText } = this.state;
+    const defaultStrings: string[] = [
+      "90's movies starring Keanu Reeves",
+      "Movies i've liked",
+      'Comedy tv shows on Netflix',
+      'The Matrix',
+      'Movies like The Matrix',
+      'Show with Michael Scott',
+    ];
+
+    if (unusedSearchText.length === 0) {
+      this.setState(
+        {
+          unusedSearchText: defaultStrings,
+        },
+        () => {
+          this.getSearchText();
+        },
+      );
+    } else {
+      this.getSearchText();
+    }
+  };
+
+  renderSearchSection = () => {
+    const { classes } = this.props;
+    const { activeSearchText, unusedSearchText } = this.state;
+
+    return (
+      <div className={classes.searchContainer}>
+        <div className={classes.searchCtaContainer}>
+          <div className={classes.movieCountContainer}>
+            <Typography
+              color="secondary"
+              variant="h2"
+              className={classes.movieCount}
+            >
+              Search
+            </Typography>
+            <Typography variant="h2"> made easy.</Typography>
+          </div>
+          <Typography variant="h4" color="textSecondary">
+            <Typist
+              key={unusedSearchText.length}
+              onTypingDone={this.resetSearchText}
+            >
+              <Typist.Delay ms={1000} />
+              {activeSearchText}
+              <Typist.Backspace
+                count={activeSearchText[0].length}
+                delay={1250}
+              />
+            </Typist>
+          </Typography>
+          <div className={classes.buttonContainer}>
+            <Button
+              size="small"
+              variant="outlined"
+              aria-label="Signup"
+              onClick={() => this.toggleAuthModal('signup')}
+              color="secondary"
+              className={classes.signupButton}
+            >
+              <PersonAdd className={classes.buttonIcon} />
+              Signup
+            </Button>
+            <Button
+              size="small"
+              variant="outlined"
+              aria-label="Login"
+              onClick={() => this.toggleAuthModal('login')}
+              className={classes.loginButton}
+            >
+              <ExitToApp className={classes.buttonIcon} />
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    const { classes } = this.props;
 
     return !this.props.isAuthed ? (
       <React.Fragment>
-        <div className={classes.container}>
-          <div className={classes.gridContainer}>
-            <Grid container spacing={1}>
-              {popular &&
-                popular.map(result => {
-                  let thing = thingsBySlug[result];
-                  return (
-                    <ItemCard
-                      key={result}
-                      userSelf={userSelf}
-                      item={thing}
-                      gridProps={{ xs: 6, sm: 6, md: 6, lg: 6 }}
-                      hoverAddToList={false}
-                      hoverDelete={false}
-                      hoverWatch={false}
-                    />
-                  );
-                })}
-            </Grid>
-          </div>
-          <div className={classes.ctaContainer}>
-            <div className={classes.movieCountContainer}>
-              <Typography
-                color="secondary"
-                variant="h2"
-                className={classes.movieCount}
-              >
-                <Odometer
-                  value={this.state.numberMovies}
-                  format="(,ddd)"
-                  theme="default"
-                  duration={5000}
-                  animation="count"
-                />
-              </Typography>
-              <Typography variant="h2"> movies &amp; counting.</Typography>
-            </div>
-            <Typography variant="h4" color="textSecondary">
-              Discover what you're not watching.
-            </Typography>
-            <div className={classes.buttonContainer}>
-              <Button
-                size="small"
-                variant="outlined"
-                aria-label="Signup"
-                onClick={() => this.toggleAuthModal('signup')}
-                color="secondary"
-                className={classes.signupButton}
-              >
-                <PersonAdd className={classes.buttonIcon} />
-                Signup
-              </Button>
-              <Button
-                size="small"
-                variant="outlined"
-                aria-label="Login"
-                onClick={() => this.toggleAuthModal('login')}
-                className={classes.loginButton}
-              >
-                <ExitToApp className={classes.buttonIcon} />
-                Login
-              </Button>
-            </div>
-          </div>
+        <div className={classes.wrapper}>
+          {this.renderTotalMoviesSection()}
+          <Divider />
+          {this.renderSearchSection()}
+          <Divider />
         </div>
         <AuthDialog
           open={this.state.authModalOpen}
