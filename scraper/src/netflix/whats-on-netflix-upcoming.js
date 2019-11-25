98ec7b027d10ae20b99ac07e9f9d94c76633c712
Christian Benincasa <chrisbenincasa@gmail.com>
2019-11-25 12:04:37 -0700

    Use proxy for new on netflix catalog scraper. Improvements to upcoming scraper

--- scraper/src/netflix/whats-on-netflix-upcoming.js
+++ scraper/src/netflix/whats-on-netflix-upcoming.js
@@ -1,10 +1,15 @@
 import request from 'request-promise';
 import cheerio from 'cheerio';
 import { USER_AGENT_STRING } from '../common/constants';
+import moment from 'moment';
+
+const TITLE_REGEX = /What's Coming to Netflix on ([A-z]+ [0-9a-z]+).*/i;
+const nameAndYearRegex = /^(.*)\s\((\d+)\)(\s?N)$/i;
 
 export const scrape = async event => {
   try {
     const url = event.url;
+    const year = event.year || moment().year();
 
     if (!url) {
       throw new Error('Need url');
@@ -18,10 +23,31 @@ export const scrape = async event => {
 
     const $ = cheerio.load(html);
 
-    const listItems = $('ul')
-      .filter((_, el) => !$(el).classList)
-      .map((_, el) =>
+    const headers = $('h4')
+      .filter((_, el) =>
         $(el)
+          .text()
+          .toLowerCase()
+          .includes('netflix on'),
+      )
+      .get();
+
+    headers.forEach(header => {
+      const $el = $(header);
+      const text = $el
+        .text()
+        .replace(/[\u2018\u2019]/g, "'")
+        .replace(/[\u201C\u201D]/g, '"');
+
+      const matches = TITLE_REGEX.exec(text);
+      if (matches && matches.length > 1) {
+        const date = moment(matches[1], 'MMMM Do');
+        console.log(date.format());
+      }
+
+      const list = $el.nextAll('ul').first();
+      if (list) {
+        const listItems = list
           .find('li')
           .map((_, li) =>
             $(li)
@@ -29,11 +55,10 @@ export const scrape = async event => {
               .text(),
           )
           .get()
-          .filter(x => x.trim().length > 0),
-      )
-      .get();
-
-    console.log(listItems);
+          .filter(x => x.trim().length > 0);
+        console.log(listItems);
+      }
+    });
   } catch (e) {
     console.error(e);
   }
