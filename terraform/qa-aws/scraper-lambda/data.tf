e498e642ef08c648fec77f558e7e31618c6f5284
Christian Benincasa <chrisbenincasa@gmail.com>
2019-12-17 10:24:20 -0700

    Cleanup some lambda TF. Use s3 object versioning. Add DLQs for every lambda

---
+++ terraform/qa-aws/scraper-lambda/data.tf
@@ -0,0 +1,16 @@
+data "aws_iam_policy" "lambda_basic_execution_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy" "lambda_s3_access_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+data "aws_iam_policy" "lambda_sqs_access_policy" {
+  arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+data "aws_s3_bucket_object" "scrapers-artifact" {
+  bucket = "teletracker-artifacts"
+  key    = "scrapers/scrapers.zip"
+}
