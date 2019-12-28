8c0e23c922bad128f6a3beef6cf5d452175544ec
Christian Benincasa <chrisbenincasa@gmail.com>
2019-12-28 13:02:45 -0700

    Fix whats-on-netflix scraper

--- scraper/src/netflix/whats-on-netflix-catalog.js
+++ scraper/src/netflix/whats-on-netflix-catalog.js
@@ -16,55 +16,72 @@ const ORIGINALS_ENDPOINT =
   'https://www.whats-on-netflix.com/wp-content/plugins/whats-on-netflix/json/originals.json';
 
 const scrapeType = async (typ, endpoint) => {
-  let [path, stream, flush] = createWriteStream(`netflix-${typ}-catalog.json`);
-  let [tmpPath, tmpStream, tmpFlush] = createWriteStream(
-    `netflix-${typ}-catalog-temp-catalog.json`,
-  );
-
-  request
-    .get(endpoint, {
-      headers: {
-        'User-Agent': USER_AGENT_STRING,
-      },
-      qs: {
-        _: moment().unix() * 1000,
-      },
-    })
-    .pipe(tmpStream);
-
-  await tmpFlush;
-
-  let blob = JSON.parse((await fs.readFile(tmpPath)).toString('utf-8'));
-
-  blob.forEach(line => {
-    stream.write(JSON.stringify(line, null, 0) + '\n');
-  });
-
-  let currentDate = moment().format('YYYY-MM-DD');
-
-  await flush;
-
-  if (isProduction()) {
-    await uploadToS3(
-      DATA_BUCKET,
-      `scrape-results/netflix/whats-on-netflix/${currentDate}/netflix-${typ}-catalog.json`,
-      path,
+  try {
+    let [path, stream, flush] = createWriteStream(
+      `netflix-${typ}-catalog.json`,
     );
+    let [tmpPath, tmpStream, tmpFlush] = createWriteStream(
+      `netflix-${typ}-catalog-temp-catalog.json`,
+    );
+
+    request
+      .get(endpoint, {
+        headers: {
+          'User-Agent': USER_AGENT_STRING,
+        },
+        qs: {
+          _: moment().unix() * 1000,
+        },
+      })
+      .pipe(tmpStream);
+
+    await tmpFlush;
+
+    let blob = JSON.parse((await fs.readFile(tmpPath)).toString('utf-8'));
+
+    blob.forEach(line => {
+      stream.write(JSON.stringify(line, null, 0) + '\n');
+    });
+
+    stream.close();
+
+    await flush;
+
+    let currentDate = moment().format('YYYY-MM-DD');
+
+    if (isProduction()) {
+      await uploadToS3(
+        DATA_BUCKET,
+        `scrape-results/netflix/whats-on-netflix/${currentDate}/netflix-${typ}-catalog.json`,
+        path,
+      );
+    }
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
 };
 
 export const scrape = async event => {
   let typ = event.type || 'all';
 
-  await Promise.all([
-    typ === 'movie' || typ === 'all'
-      ? scrapeType('movie', MOVIES_ENDPOINT)
-      : Promise.resolve(),
-    typ === 'tv' || typ === 'all'
-      ? scrapeType('tv', SHOWS_ENDPOINT)
-      : Promise.resolve(),
-    typ === 'originals' || typ === 'all'
-      ? scrapeType('originals', ORIGINALS_ENDPOINT)
-      : Promise.resolve(),
-  ]);
+  console.log('Scraping whats-on-netflix type ' + typ);
+
+  try {
+    await Promise.all([
+      typ === 'movie' || typ === 'all'
+        ? scrapeType('movie', MOVIES_ENDPOINT)
+        : Promise.resolve(),
+      typ === 'tv' || typ === 'all'
+        ? scrapeType('tv', SHOWS_ENDPOINT)
+        : Promise.resolve(),
+      typ === 'originals' || typ === 'all'
+        ? scrapeType('originals', ORIGINALS_ENDPOINT)
+        : Promise.resolve(),
+    ]);
+  } catch (e) {
+    console.error(e);
+
+    throw e;
+  }
 };
