51c362f9465ae521166a62cb56305d40dc8454c1
Christian Benincasa <chrisbenincasa@gmail.com>
2019-12-31 11:29:07 -0700

    Codebuild stuff

--- codebuild/build.sh
+++ codebuild/build.sh
@@ -3,7 +3,7 @@
 # cp ../scala-server/build.sbt build.sbt
 rsync -av --progress --exclude=target/ --exclude=.bloop ../scala-server/project ../scala-server/build.sbt  .
 
-docker build -t 302782651551.dkr.ecr.us-west-1.amazonaws.com/teletracker-build/server . -f Dockerfile.alpine
+docker build -t 302782651551.dkr.ecr.us-west-1.amazonaws.com/teletracker-build/server . -f Dockerfile
 
 rm build.sbt
 rm -rf project/
