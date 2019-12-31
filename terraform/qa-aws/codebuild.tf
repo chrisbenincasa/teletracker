708e29486f6b345390e0545a13c5b61de3aa526c
Christian Benincasa <chrisbenincasa@gmail.com>
2019-12-31 13:01:50 -0700

    Split consumer and server buildspec

--- terraform/qa-aws/codebuild.tf
+++ terraform/qa-aws/codebuild.tf
@@ -41,7 +41,7 @@ resource "aws_codebuild_project" "scraper-codebuild" {
 }
 
 resource "aws_codebuild_project" "server-codebuild" {
-  name         = "Consumer-Build"
+  name         = "Server-Build"
   service_role = "arn:aws:iam::302782651551:role/ServerCodeBuildRole"
 
   artifacts {
@@ -49,12 +49,10 @@ resource "aws_codebuild_project" "server-codebuild" {
   }
 
   environment {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    //    image                       = "302782651551.dkr.ecr.us-west-1.amazonaws.com/teletracker-build/server:latest"
+    compute_type    = "BUILD_GENERAL1_SMALL"
     image           = "aws/codebuild/standard:3.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
-    //    image_pull_credentials_type = "SERVICE_ROLE"
   }
 
   source {
@@ -64,6 +62,43 @@ resource "aws_codebuild_project" "server-codebuild" {
     report_build_status = false
     git_clone_depth     = 1
     buildspec           = "buildspec.server.yml"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "codebuild-logs"
+      status     = "ENABLED"
+    }
+
+    s3_logs {
+      encryption_disabled = false
+      status              = "DISABLED"
+    }
+  }
+}
+
+resource "aws_codebuild_project" "consumer-codebuild" {
+  name         = "Consumer-Build"
+  service_role = "arn:aws:iam::302782651551:role/ServerCodeBuildRole"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:3.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+    type                = "GITHUB"
+    location            = "https://github.com/chrisbenincasa/teletracker.git"
+    insecure_ssl        = false
+    report_build_status = false
+    git_clone_depth     = 1
+    buildspec           = "buildspec.consumer.yml"
   }
 
   logs_config {
