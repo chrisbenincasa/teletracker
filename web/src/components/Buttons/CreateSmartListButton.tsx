78855be443ff537f1c433df89d65bf923b27becf
Mark D'Avella <markdavella@gmail.com>
2020-03-16 23:35:45 -0400

    ux updates

--- web/src/components/Buttons/CreateSmartListButton.tsx
+++ web/src/components/Buttons/CreateSmartListButton.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { IconButton, Typography, Tooltip } from '@material-ui/core';
+import { Button, Tooltip } from '@material-ui/core';
 import { AddCircle } from '@material-ui/icons';
+import { useWidth } from '../../hooks/useWidth';
 
 interface Props {
   onClick: () => void;
 }
 
 export default function CreateSmartListButton(props: Props) {
+  const width = useWidth();
+  const isMobile = ['xs', 'sm'].includes(width);
+
   return (
-    <Tooltip title="Save as List" aria-label="save-as-list" placement="top">
-      <IconButton onClick={props.onClick}>
-        <AddCircle />
-        <Typography variant="srOnly">Save as List</Typography>
-      </IconButton>
+    <Tooltip
+      title="Save this search as a dynamic list"
+      aria-label="save-as-list"
+      placement="top"
+    >
+      <Button
+        size="small"
+        onClick={props.onClick}
+        variant="contained"
+        color="primary"
+        aria-label="Create Smart List"
+        startIcon={<AddCircle />}
+        fullWidth={isMobile}
+      >
+        Create Smart List
+      </Button>
     </Tooltip>
   );
 }
