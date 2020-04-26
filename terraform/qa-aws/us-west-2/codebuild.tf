8f0fd3c28336ef1171fe94147177c3ebac6bf3e1
Christian Benincasa <chrisbenincasa@gmail.com>
2020-04-26 09:38:41 -0600

    Add frontend PR build

--- terraform/qa-aws/us-west-2/codebuild.tf
+++ terraform/qa-aws/us-west-2/codebuild.tf
@@ -178,6 +178,58 @@ resource "aws_codebuild_webhook" "consumer-codebuild-github-hook" {
   }
 }
 
+resource "aws_codebuild_project" "frontend-pr-codebuild" {
+  name         = "Frontend-PR-Build"
+  service_role = "arn:aws:iam::302782651551:role/ServerCodeBuildRole"
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type    = "BUILD_GENERAL1_SMALL"
+    image           = "aws/codebuild/standard:3.0"
+    type            = "LINUX_CONTAINER"
+    privileged_mode = true
+  }
+
+  source {
+    type                = "GITHUB"
+    location            = "https://github.com/chrisbenincasa/teletracker.git"
+    insecure_ssl        = false
+    report_build_status = false
+    git_clone_depth     = 1
+    buildspec           = "buildspec.web.yml"
+  }
+
+  logs_config {
+    cloudwatch_logs {
+      group_name = "codebuild-logs"
+      status     = "ENABLED"
+    }
+
+    s3_logs {
+      encryption_disabled = false
+      status              = "DISABLED"
+    }
+  }
+
+  cache {
+    type  = "LOCAL"
+    modes = ["LOCAL_CUSTOM_CACHE"]
+  }
+}
+
+resource "aws_codebuild_webhook" "frontend-pr-codebuild-github-hook" {
+  project_name = aws_codebuild_project.frontend-pr-codebuild.name
+
+  filter_group {
+    filter {
+      pattern = "PULL_REQUEST_CREATED,PULL_REQUEST_UPDATED,PULL_REQUEST_REOPENED"
+      type    = "EVENT"
+    }
+  }
+}
 
 resource "aws_codebuild_project" "build-image-codebuild" {
   name         = "Build-Image-Build"
