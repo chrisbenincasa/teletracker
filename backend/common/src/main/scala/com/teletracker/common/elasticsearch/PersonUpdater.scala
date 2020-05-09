628de00d949ff27721f2fc16969a823da1e0a155
Christian Benincasa <chrisbenincasa@gmail.com>
2020-05-09 17:47:39 -0600

    Update person mappings on index/update

--- backend/common/src/main/scala/com/teletracker/common/elasticsearch/PersonUpdater.scala
+++ backend/common/src/main/scala/com/teletracker/common/elasticsearch/PersonUpdater.scala
@@ -1,7 +1,13 @@
 package com.teletracker.common.elasticsearch
 
 import com.teletracker.common.config.TeletrackerConfig
-import com.teletracker.common.elasticsearch.model.EsPerson
+import com.teletracker.common.db.model.ItemType
+import com.teletracker.common.elasticsearch.lookups.ElasticsearchExternalIdMappingStore
+import com.teletracker.common.elasticsearch.model.{
+  EsExternalId,
+  EsItem,
+  EsPerson
+}
 import com.teletracker.common.util.Functions._
 import io.circe.syntax._
 import javax.inject.Inject
@@ -13,24 +19,42 @@ import org.elasticsearch.common.xcontent.{
   XContentHelper,
   XContentType
 }
+import org.slf4j.LoggerFactory
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.control.NonFatal
 
 class PersonUpdater @Inject()(
   elasticsearchExecutor: ElasticsearchExecutor,
-  teletrackerConfig: TeletrackerConfig
+  teletrackerConfig: TeletrackerConfig,
+  idMappingLookup: ElasticsearchExternalIdMappingStore
 )(implicit executionContext: ExecutionContext)
     extends ElasticsearchAccess {
 
+  private val logger = LoggerFactory.getLogger(getClass)
+
   def insert(person: EsPerson): Future[IndexResponse] = {
+    val updateMappingFut = updateMappings(person)
     val indexRequest = getIndexRequest(person)
-    elasticsearchExecutor.index(indexRequest)
+    val indexFut = elasticsearchExecutor.index(indexRequest)
+
+    for {
+      _ <- updateMappingFut
+      result <- indexFut
+    } yield result
   }
 
   def update(
     person: EsPerson,
     refresh: Boolean = false
   ): Future[UpdateResponse] = {
-    elasticsearchExecutor.update(getUpdateRequest(person, refresh))
+    val updateMappingFut = updateMappings(person)
+    val updateFut =
+      elasticsearchExecutor.update(getUpdateRequest(person, refresh))
+
+    for {
+      _ <- updateMappingFut
+      result <- updateFut
+    } yield result
   }
 
   def getIndexJson(person: EsPerson) = {
@@ -70,5 +94,30 @@ class PersonUpdater @Inject()(
         XContentType.JSON
       )
       .applyIf(refresh)(_.setRefreshPolicy(RefreshPolicy.IMMEDIATE))
+  }
+
+  private def updateMappings(item: EsPerson): Future[Unit] = {
+    item.external_ids
+      .filter(_.nonEmpty)
+      .map(externalIds => {
+        val mappings = externalIds
+          .filter(isValidExternalIdForMapping)
+          .map(externalId => {
+            (externalId, ItemType.Person) -> item.id
+          })
+
+        idMappingLookup.mapExternalIds(mappings.toMap).recover {
+          case NonFatal(e) =>
+            logger.warn(
+              s"Error while attempting to map external Ids for id = ${item.id}",
+              e
+            )
+        }
+      })
+      .getOrElse(Future.unit)
+  }
+
+  private def isValidExternalIdForMapping(externalId: EsExternalId) = {
+    externalId.id.nonEmpty && externalId.id != "0"
   }
 }
