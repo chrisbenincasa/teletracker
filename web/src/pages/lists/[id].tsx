d96b1f7c40c2ee47782c5ae926ef51ece99049b8
Mark D'Avella <markdavella@gmail.com>
2020-06-15 13:05:15 -0400

    first pass

--- web/src/pages/lists/[id].tsx
+++ web/src/pages/lists/[id].tsx
@@ -37,11 +37,51 @@ function ListDetailWrapper(props: Props) {
   );
 
   let listName = list?.name || '';
+  const domain = process.env.REACT_APP_TELETRACKER_BASE_URL;
 
   return (
     <React.Fragment>
       <Head>
-        <title>{listName !== '' ? 'List - ' + listName : 'Not Found'}</title>
+        <title>
+          {listName !== ''
+            ? `List - ${listName} | Where to stream, rent, or buy`
+            : 'Not Found'}
+        </title>
+        <meta
+          name="title"
+          property="og:title"
+          content={`${listName} | Where to stream, rent, or buy`}
+        />
+        <meta
+          name="description"
+          property="og:description"
+          content={`${listName}. Find out where to stream, rent, or buy content on ${domain}.`}
+        />
+        {/* <meta name="image" property="og:image" content={imageURL} />
+          <meta
+            property="og:type"
+            content={item?.type === 'movie' ? 'video.movie' : 'video.tv_show'}
+          />
+          <meta property="og:image:type" content="image/jpg" />
+          <meta property="og:image:width" content={imageWidth} />
+          <meta property="og:image:height" content={imageHeight} /> */}
+        <meta property="og:url" content={`${domain}${router.asPath}`} />
+        <meta name="twitter:card" content={listName} />
+        <meta
+          name="twitter:title"
+          content={`${listName} - Where to Stream, Rent, or Buy Online`}
+        />
+        <meta
+          name="twitter:description"
+          content={`${listName} - Where to Stream, Rent, or Buy Online`}
+        />
+        {/* <meta name="twitter:image" content={imageURL} /> */}
+        <meta name="twitter:domain" content={domain} />
+        <meta
+          name="keywords"
+          content={`list, stream, streaming, rent, buy, watch, track`}
+        />
+        <link rel="canonical" href={`${domain}${router.asPath}`} />
       </Head>
       <AppWrapper>
         <ListDetail preloaded={props.preloaded} />
@@ -58,21 +98,19 @@ ListDetailWrapper.getInitialProps = async ctx => {
     let token = await currentUserJwt();
 
     // Load the list and a few items
-    let listResponseFut: Promise<TeletrackerResponse<ApiList>> = TeletrackerApi.instance.getList(token, ctx.query.id)
+    let listResponseFut: Promise<TeletrackerResponse<
+      ApiList
+    >> = TeletrackerApi.instance.getList(token, ctx.query.id);
 
     let itemsResponseFut: Promise<TeletrackerResponse<
       ApiItem[]
-    >> = TeletrackerApi.instance.getListItems(
-      token,
-      ctx.query.id,
-      {
-        sort: filterParams.sortOrder,
-        itemTypes: filterParams.itemTypes,
-        genres: filterParams.genresFilter,
-        networks: filterParams.networks,
-        limit: 6,
-      }
-    );
+    >> = TeletrackerApi.instance.getListItems(token, ctx.query.id, {
+      sort: filterParams.sortOrder,
+      itemTypes: filterParams.itemTypes,
+      genres: filterParams.genresFilter,
+      networks: filterParams.networks,
+      limit: 6,
+    });
 
     // Load page metadata (needed for filter calculation)
     let metadataFut = TeletrackerApi.instance.getMetadata();
@@ -101,15 +139,17 @@ ListDetailWrapper.getInitialProps = async ctx => {
             list: list,
           }),
         ),
-        ctx.store.dispatch(retrieveListItemsSucceeded({
-          listId: list.id,
-          items: itemsResponse.data!.data.map(ItemFactory.create),
-          append: false,
-          paging: itemsResponse.data!.paging,
-          forFilters: list.isDynamic
-            ? { ...defaultFilters, ...filterParams }
-            : filterParams,
-        })),
+        ctx.store.dispatch(
+          retrieveListItemsSucceeded({
+            listId: list.id,
+            items: itemsResponse.data!.data.map(ItemFactory.create),
+            append: false,
+            paging: itemsResponse.data!.paging,
+            forFilters: list.isDynamic
+              ? { ...defaultFilters, ...filterParams }
+              : filterParams,
+          }),
+        ),
         ctx.store.dispatch(peopleFetchSuccess(people)),
         ctx.store.dispatch(
           loadMetadataSuccess({
