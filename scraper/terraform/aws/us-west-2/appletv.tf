a8cef9df1e20d265159a0b284f9724307036b616
Christian Benincasa <chrisbenincasa@gmail.com>
2020-09-19 09:27:15 -0400

    Standup apple tv crawler infra

---
+++ scraper/terraform/aws/us-west-2/appletv.tf
@@ -0,0 +1,38 @@
+variable "apple_tv_catalog_crawler_version" {
+  type = string
+}
+
+module "appletv_crawler" {
+  source = "./modules/crawler"
+
+  crawler_image = var.crawler_image
+  image_version = var.apple_tv_catalog_crawler_version
+
+  name         = "appletv"
+  spider_name  = "appletv"
+  cluster_name = data.aws_ecs_cluster.ecs-cluster.cluster_name
+
+  outputs = [
+    "s3://${data.aws_s3_bucket.data_bucket.id}/scrape-results/%(canonical_name)s/catalog/%(today)s/%(version)s/items_%(now)s.jl:jl",
+    "sqs://${replace(data.aws_sqs_queue.scrape_item_output_queue.id, "https://", "")}:sqs"
+  ]
+
+  extra_args = [
+    "-sEMPTY_RESPONSE_RECORDER_ENABLED=True",
+    "-sAUTOTHROTTLE_TARGET_CONCURRENCY=8",
+    "-sTELNETCONSOLE_HOST=0.0.0.0"
+  ]
+
+  dynamodb_output_table = aws_dynamodb_table.crawls.name
+  redis_host            = "crawl_store.cache.internal.qa.teletracker.tv"
+
+  schedule = ["cron(0 7 ? * */3 *)",
+  "cron(0 7 1 * ? *)"]
+  scheduled_task_count = 3
+
+  max_spider_count = 3
+
+  gen_service = false
+  memory      = 512
+  fargate     = true
+}
