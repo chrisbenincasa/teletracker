3d1499b3257ac7e1f57c301d2a9fb9800dcbf9a1
Christian Benincasa <chrisbenincasa@gmail.com>
2020-10-03 08:34:48 -0400

    Deploy consumers, create non-distributed amazon crawler for testing

--- scraper/terraform/aws/us-west-2/amazon.tf
+++ scraper/terraform/aws/us-west-2/amazon.tf
@@ -36,3 +36,31 @@ module "amazon_crawler" {
   memory      = 512
   fargate     = true
 }
+
+module "amazon_single_crawler" {
+  source = "./modules/crawler"
+
+  crawler_image = var.crawler_image
+  image_version = var.amazon_catalog_crawler_version
+
+  name         = "amazon_crawler_single"
+  spider_name  = "amazon"
+  cluster_name = data.aws_ecs_cluster.ecs-cluster.cluster_name
+
+  outputs = [
+    "s3://${data.aws_s3_bucket.data_bucket.id}/scrape-results/%(canonical_name)s/catalog/%(version)s/items_%(now)s.jl:jl"
+  ]
+
+  extra_args = [
+    "-sEMPTY_RESPONSE_RECORDER_ENABLED=True",
+    "-sAUTOTHROTTLE_TARGET_CONCURRENCY=8",
+    "-sTELNETCONSOLE_HOST=0.0.0.0"
+  ]
+
+  dynamodb_output_table = aws_dynamodb_table.crawls.name
+  redis_host            = "crawl_store.cache.internal.qa.teletracker.tv"
+
+  gen_service = false
+  memory      = 512
+  fargate     = true
+}
